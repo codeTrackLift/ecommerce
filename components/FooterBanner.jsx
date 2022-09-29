d87571a57543aed140b14fba69a50e6570baa286
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-09-29 18:23:32 -0400

    add products, working on footer

---
+++ components/FooterBanner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
+
+const FooterBanner = ({ footerBanner: { discounter, largeText1, largeText2, saleTime, discount, smallText, midText, product, buttonText, image } }) => {
+  return (
+    <div className='footer-banner-container'>
+      <div className='banner-desc'>
+        <div className='left'>
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className='right'>
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterBanner
