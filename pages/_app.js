d10841a3fed74f14fa6b68e431a14270e5cabbc9
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-10-02 19:42:04 -0400

    adjust hero img

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { Toaster } from 'react-hot-toast';
 import { StateContext } from '../context/StateContext';
 import { Layout } from '../components';
-import '../styles/globals.css'
+import '../styles/banner.css'
+import '../styles/cart.css'
+import '../styles/footer.css'
+import '../styles/hero.css'
+import '../styles/global.css'
+import '../styles/product.css'
+import '../styles/success.css'
 
 function MyApp({ Component, pageProps }) {
   return (
