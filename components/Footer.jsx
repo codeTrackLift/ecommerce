a361b23bd308531b34b382b34570e0c503d00404
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-10-06 18:19:30 -0400

    add footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineTwitter, AiFillYoutube } from 'react-icons/ai';
+
+const urlTwitter = 'https://twitter.com/codeTrackLift'
+const urlYoutube = 'https://www.youtube.com/channel/UCVDOFoM5NXYrPoC02lbNJ-Q'
 
 const Footer = () => {
   return (
     <div className='footer-container'>
       <p>2022 E-commerce Store</p>
       <p className='icons'>
-        <AiFillInstagram />
-        <AiOutlineTwitter />
+        <a href={urlYoutube} target='_blank' rel='noreferrer'>
+          <AiFillYoutube />
+        </a>
+        <a href={urlTwitter} target='_blank' rel='noreferrer'>
+          <AiOutlineTwitter />
+        </a>
       </p>
     </div>
   )
